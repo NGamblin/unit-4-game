26c3f9e0176abbc1029dd4461944b1cd41ad707d
NGamblin <nickgamblin@gmail.com>
2019-04-08 22:52:53 -0500

    logic working // should decrease global vars

--- assets/game.js
+++ assets/game.js
@@ -4,11 +4,11 @@ var crystal3Value;
 var crystal4Value;
 var goalNumb;
 var scoreNumb = 0;
-var Wins = 0;
+var wins = 0;
 var losses = 0;
 
 
-function beginReset() {
+function start() {
     var min=19;
     var max=120;
     goalNumb = Math.floor(Math.random() * (+max - +min) + +min);
@@ -18,11 +18,43 @@ function beginReset() {
     crystal4Val = Math.floor(Math.random() * 13) +1;
     $("#displayGoal").text(goalNumb);
     $("#displayScore").text(scoreNumb);
-    $("#displayWins").text();
-    $("#displayLosses").text();
+    $("#displayWins").text(wins);
+    $("#displayLosses").text(losses);
     console.log(goalNumb);
     console.log(crystal1Val);
+    console.log(crystal1Val, crystal2Val, crystal3Val, crystal4Val)
 }
+
+function reset() {
+  var min=19;
+  var max=120;
+  goalNumb = Math.floor(Math.random() * (+max - +min) + +min);
+  scoreNumb = 0;
+  crystal1Val = Math.floor(Math.random() * 12) +1;
+  crystal2Val = Math.floor(Math.random() * 12) +1;
+  crystal3Val = Math.floor(Math.random() * 12) +1;
+  crystal4Val = Math.floor(Math.random() * 12) +1;
+  $("#displayGoal").text(goalNumb);
+  $("#displayScore").text(scoreNumb);
+  $("#displayWins").text(wins);
+  $("#displayLosses").text(losses);
+  console.log(goalNumb);
+  console.log(crystal1Val);
+}
+
+function evalWin() {
+  // debugger;
+  if (scoreNumb === goalNumb) {
+    wins++;
+    $("#displayWins").text(wins);
+    reset()
+ } else if (scoreNumb > goalNumb) {
+   losses++;
+   $("#displayLosses").text(losses);
+   reset();
+  }
+}
+
 
 //=============================================================================
 
@@ -33,6 +65,7 @@ $("#crystal1").on("click", function() {
     scoreNumb = scoreNumb + crystal1Val;
     console.log(crystal1Val)
     $("#displayScore").text(scoreNumb);
+    evalWin();
 
   });
 
@@ -41,7 +74,7 @@ $("#crystal1").on("click", function() {
     scoreNumb = scoreNumb + crystal2Val;
     console.log(crystal2Val)
     $("#displayScore").text(scoreNumb)
-
+    evalWin();
   });
 
   $("#crystal3").on("click", function() {
@@ -49,7 +82,7 @@ $("#crystal1").on("click", function() {
     scoreNumb = scoreNumb + crystal3Val;
     console.log(crystal3Val)
     $("#displayScore").text(scoreNumb)
-
+    evalWin();
   });
 
   $("#crystal4").on("click", function() {
@@ -57,7 +90,9 @@ $("#crystal1").on("click", function() {
     console.log(crystal4Val)
     scoreNumb = scoreNumb + crystal4Val;
     $("#displayScore").text(scoreNumb)
-
+    
   });
+//======================================================================
 
-  beginReset();
+
+  start();
