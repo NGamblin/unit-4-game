7337aabe6ac113668247230aa9a63094987b531b
NGamblin <nickgamblin@gmail.com>
2019-04-08 21:53:15 -0500

    added bootstrap

--- assets/game.js
+++ assets/game.js
@@ -4,6 +4,8 @@ var crystal3Value;
 var crystal4Value;
 var goalNumb;
 var scoreNumb = 0;
+var Wins = 0;
+var losses = 0;
 
 
 function beginReset() {
@@ -14,17 +16,23 @@ function beginReset() {
     crystal2Val = Math.floor(Math.random() * 13) +1;
     crystal3Val = Math.floor(Math.random() * 13) +1;
     crystal4Val = Math.floor(Math.random() * 13) +1;
-    $("#goal-display").text(goalNumb);
+    $("#displayGoal").text(goalNumb);
+    $("#displayScore").text(scoreNumb);
+    $("#displayWins").text();
+    $("#displayLosses").text();
     console.log(goalNumb);
     console.log(crystal1Val);
 }
+
+//=============================================================================
+
 
 $("#crystal1").on("click", function() {
     
     console.log("clicked1");
     scoreNumb = scoreNumb + crystal1Val;
     console.log(crystal1Val)
-    $("#score-display").text(scoreNumb);
+    $("#displayScore").text(scoreNumb);
 
   });
 
@@ -32,7 +40,7 @@ $("#crystal1").on("click", function() {
     console.log("clicked2");
     scoreNumb = scoreNumb + crystal2Val;
     console.log(crystal2Val)
-    $("#score-display").text(scoreNumb)
+    $("#displayScore").text(scoreNumb)
 
   });
 
@@ -40,7 +48,7 @@ $("#crystal1").on("click", function() {
     console.log("clicked3");
     scoreNumb = scoreNumb + crystal3Val;
     console.log(crystal3Val)
-    $("#score-display").text(scoreNumb)
+    $("#displayScore").text(scoreNumb)
 
   });
 
@@ -48,7 +56,7 @@ $("#crystal1").on("click", function() {
     console.log("clicked4");
     console.log(crystal4Val)
     scoreNumb = scoreNumb + crystal4Val;
-    $("#score-display").text(scoreNumb)
+    $("#displayScore").text(scoreNumb)
 
   });
 
